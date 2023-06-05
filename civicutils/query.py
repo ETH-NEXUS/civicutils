6fa8490826b42d4532c27436cab3e95c2a34b443
ahanns <hanns@nexus.ethz.ch>
2023-06-05 09:13:10 +0200

    [FIX] quoting issue

--- civicutils/query.py
+++ civicutils/query.py
@@ -235,7 +235,7 @@ def reformat_civic(results, identifier_type="entrez_symbol"):
                         # Sanity checks that only "PREDICTIVE" evidences have drugs associated
                         # Submitted evidence items can fulfill having "PREDICTIVE" evidence type and no drugs ("NULL")
                         if (evidence_type != "PREDICTIVE") and (drugs != ["NULL"]):
-                            raise ValueError("Only evidences of type "PREDICTIVE" can have drugs associated!")
+                            raise ValueError("Only evidences of type 'PREDICTIVE' can have drugs associated!")
                             
                 # Iterate through drugs to add evidences associated to them
                 #   For non-Predictive evidences or Predictive with empty drugs, drugs=["NULL"]
